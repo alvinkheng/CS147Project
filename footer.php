809fab27ecfd794aad7d713a499a6167834d31c4
Francisco Guzman <fguzman1@stanford.edu>
2012-10-29 15:40:30 -0700

    changes to file names to better reflect our new navigation

--- footer.php
+++ footer.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 
-<div data-role="footer" data-id="samebar" class="nav-glyphish-example" data-position="fixed" data-tap-toggle="false">
-  <div data-role="navbar" class="nav-glyphish-example" data-grid="c">    
+<div data-role="footer" data-id="samebar" data-position="fixed">
+  <div data-role="navbar" data-grid="c">    
     <ul>
-      <li><a href="index.php" id="home" data-icon="custom" class=<?= $select0; ?>>Feed</a></li>
-      <li><a href="dashboard.php" id="dashboard" data-icon="custom" class=<?= $select1; ?>>Dashboard</a></li>
-      <li><a href="addStatus.php" id="add" data-icon="custom" class=<?= $select2; ?>>Add Status</a></li>
-      <li><a href="settings.php" id="settings" data-icon="custom" class=<?= $select3; ?>>Settings</a></li>
+      <li><a href="personal.php" id="personal" data-icon="custom" class=<?= $select0; ?>></a></li>
+      <li><a href="global.php" id="global" data-icon="custom" class=<?= $select1; ?>></a></li>
     </ul>
   </div>
 </div>
