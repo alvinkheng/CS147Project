ed1dd41ea7cbcb5eb6b138d355ff847a27cfcbb3
alvinkheng <alvin.k.heng@gmail.com>
2012-10-30 14:46:02 -0700

    cleaned up some header code

--- header.php
+++ header.php
@@ -4,33 +4,32 @@
   function presentStatus() {
   $.mobile.changePage( "addStatus.php", { transition: "slideup"} );
   }
-
+  
   function closeStatus() {
   $.mobile.changePage( "personal.php", { transition: "slidedown"} );
   }
 </script>
 
-<div data-role="header" data-position="fixed" data-id="sameheader">
   <?php
      if ($isModal) {
      ?>
-  <a data-icon="delete" onclick="closeStatus()">Cancel</a>
-  <?php
-     }
-     ?>
-  <h1><?= $title; ?></h1>
   
-  <?php
-     if ($isModal) {
-     ?>
-  <a data-icon="check" data-theme="b" onclick="closeStatus()">Add</a>
-  <?php
-     } else {
-     ?>
-  <a data-icon="plus" data-theme="b"
-  class="ui-btn-right" onclick="presentStatus()">Add</a>
-  <?php
-     }
-     ?>   
-  
-</div><!-- /header -->
+  <div data-role="header">
+    <a data-icon="delete" onclick="closeStatus()">Cancel</a>
+    <a data-icon="check" data-theme="b" onclick="closeStatus()">Add</a>
+    
+    <?php
+       } else {
+       ?>
+    
+    <div data-role="header" data-position="fixed" data-id="sameheader">
+      <a data-icon="plus" data-theme="b" class="ui-btn-right" onclick="presentStatus()">Add</a>
+      
+      <?php
+	 }
+	 ?>
+      
+      <h1><?= $title; ?></h1>
+      
+    </div>
+    
