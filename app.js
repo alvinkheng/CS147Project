5df4bb80ab0576b0cfbaeb4db330ed9c35414cf4
Sheldon Chang <sheldon0789@gmail.com>
2012-10-31 19:53:24 -0700

    got save settings to work

--- app.js
+++ app.js
@@ -27,7 +27,21 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
+//dummy data
+var currUser = {
+  'name': 'Katherine Chen',
+  'email': 'kchen12@stanford.edu',
+  'password': 'hello',
+  'gender': 'f',
+  'privacy': 'friends',
+  'location': 'on'
+}
 
+var globalPosts = [
+  { text: 'I had the best day ever!', emotion: 'happy', location: 'Boston, MA'},
+  { text: 'Did not do so hot on my Chem exam...', emotion: 'sad', location: 'Stanford, CA'},
+  { text: 'Normal day, not much going on', emotion: 'neutral', location: 'Houston, TX'}
+]
 
 app.configure('development', function(){
   app.use(express.errorHandler());
@@ -35,7 +49,26 @@ app.configure('development', function(){
 
 app.get('/', routes.home);
 app.get('/globaldashboard', routes.globaldash)
-app.get('/settings', routes.settings)
+app.get('/settings', routes.settings) 
+app.post('/save-settings', function(req, res) {
+  console.log('got settings')
+  var params = req.body;
+
+  currUser['name'] = params['name']
+  currUser['email'] = params['email']
+  if(params['pass']) currUser['password'] = params['pass']
+  currUser['gender'] = params['gender']
+  currUser['privacy'] = params['privacy']
+  currUser['location'] = params['flip-s']
+
+  res.render('global.html')
+}) 
+
+app.get('/user-info', function(req, res) {
+  console.log(JSON.stringify(currUser))
+  res.write(JSON.stringify(currUser))
+  res.end()
+})
 
 app.get('/users', user.list);
 
