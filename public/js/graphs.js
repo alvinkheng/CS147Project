f32a4cc794e014f73c981553ed0117731d031638
Sheldon Chang <sheldon0789@gmail.com>
2012-11-08 23:46:46 -0800

    added d3

---
+++ public/js/graphs.js
@@ -0,0 +1,30 @@
+$(function() {
+	drawGraphs()
+})
+
+function drawGraphs() {
+	var w = 400;
+	var h = 100;
+	var barPadding = 1;
+
+	var svg = d3.select("#emotion-graph")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
+
+
+    var dataset = [62, 18, 20]
+
+    svg.selectAll("rect")
+	   .data(dataset)
+	   .enter()
+	   .append("rect")
+	   .attr("x", function(d, i) {
+    	return i * (w / dataset.length);	})
+	   .attr("y", function(d) {
+  		  return h - d;  })
+	   .attr("width", w / dataset.length - barPadding)
+	   .attr("height", function(d) {
+   		 return d;	})
+	   .attr('fill', 'orange')
+}
