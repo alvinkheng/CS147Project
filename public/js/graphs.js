6176f03aa81d717b87c6a86c35c688b9740abf08
Sheldon Chang <sheldon0789@gmail.com>
2012-11-27 22:18:04 -0800

    started line graph

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -1,21 +1,80 @@
+
+function drawLineGraph(statuses, startDate) {
+	$('#time-graph').html('')
+	var emotionScale = { 'excited': 2, 'happy':1, 'neutral': 0, 'sad':-1, 'angry':-2}
+
+	var w = $(window).width();
+  	var h = 150;
+  	var p = 20
+  	var bottomMargin = 35;
+  	var topMargin = 10;
+  	var margin = 40;
+
+  	var minStartDate = new Date((new Date()).getTime()-1000*60*60*24*7);	// last week, testing
+  	//loop through data
+  	var relevantData = []
+  	for(var i=0; i < statuses.length; i++) {
+  		var curr = statuses[i]
+  		var currDate = new Date(curr['date'])
+  		if(currDate > minStartDate) {
+  			var currEmotion = curr['emotion']
+  			if(currEmotion in emotionScale) 
+ 	 			relevantData.push({ 'date': currDate, 'score': emotionScale[currEmotion] })
+  		}
+  	}
+  	console.log("Filtered data for line graph: "+JSON.stringify(relevantData))
+
+  	// set up axes
+  	var minDate = relevantData[(relevantData.length - 1)]['date']
+  	var maxDate = relevantData[0]['date']
+  	var y = d3.scale.linear().domain([-2, 2]).range([h, 0])
+    var x = d3.time.scale().domain([minDate, maxDate]).range([0, w-p*2]);
+
+	var svg = d3.select("#time-graph")
+	  	.append("svg")
+	  	.attr("width", w)
+	  	.attr("height", h+bottomMargin+topMargin)
+
+	var g = svg.append("svg:g")
+    	.attr("transform", "translate(" + 2*p + "," + p + ")");
+			
+	var line = d3.svg.line()
+	    .x(function(d) { return x(d.date); })
+	    .y(function(d) { return y(d.score); })
+
+	g.append("svg:path").attr("d", line(relevantData));
+
+	g.selectAll("circle.line")
+	    .data(relevantData)
+	    .enter().append("svg:circle")
+	    .attr("class", "line")
+	    .attr("cx", function(d) { return x(d.date) })
+	    .attr("cy", function(d) { return y(d.score) })
+	    .attr("r", 3.5)
+}
+
 function drawGraphs(statuses) {
-  console.log("statuses: "+JSON.stringify(statuses))
+ 	console.log('inside drawGraphs!'+JSON.stringify(statuses))
+	$('#emotion-graph').html('')
+
   var w = $(window).width();
   var h = 100;
   var bottomMargin = 35;
+  var topMargin = 10;
   var barPadding = 1;
 
   var svg = d3.select("#emotion-graph")
 	  .append("svg")
+	  .attr('class', 'emotion')
 	  .attr("width", w)
-	  .attr("height", h+bottomMargin);
+	  .attr("height", h+bottomMargin+topMargin);
 
   var emotionIndex = { 'excited':0, 'happy':1, 'neutral':2, 'sad':3, 'angry':4, 'angel':5, 'devil':6, 'disappoint':7, 'laugh':8, 'surprised':9}
   var emotions = []
   for(var key in emotionIndex) {
   	emotions.push(key)
   }
-  console.log("emotions: "+JSON.stringify(emotions))
+  var colors = ['orange', 'pink', 'gray', 'blue', 'red', 'silver', 'maroon', 'black', 'salmon', 'coral']
 
   var dataset = [0,0,0,0,0,0,0,0,0,0]
 
@@ -38,11 +97,13 @@ function drawGraphs(statuses) {
 	 .attr("x", function(d, i) {
 	  return i * (w / dataset.length);    })
 	 .attr("y", function(d) {
-		return h - d*scale;  })
+		return h +topMargin - d*scale;  })
 	 .attr("width", w / dataset.length - barPadding)
 	 .attr("height", function(d) {
 	   return d * scale})
-	 .attr('fill', 'orange')
+	 .attr('fill', function(d, i) {
+		return colors[i % colors.length]	 	
+	 })
 
 	svg.selectAll("image")
 	  	.data(dataset)
@@ -56,7 +117,7 @@ function drawGraphs(statuses) {
 	    .attr("height", 39)
 		.attr("x", function(d, i) {
 		  return i * (w / dataset.length);    })
-		.attr("y", h)
+		.attr("y", h+topMargin)
 		.attr("width", w / dataset.length - barPadding)
 
 	svg.selectAll("text")
@@ -71,7 +132,7 @@ function drawGraphs(statuses) {
     	  	return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
  	  	})
 	   	.attr("y", function(d) {
-       		return h - (d * scale) + 17; 
+       		return h - (d * scale) + topMargin+ 15; 
     	})
    		.attr("font-family", "sans-serif")
    		.attr("font-size", "11px")
